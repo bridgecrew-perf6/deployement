261cc0bd959af8106fcf6a2105a4dc247d60ab6a
amdcdac08 <amdcdac08@amdcdac12.iiht.tech>
2022-06-16 10:16:39 +0530

    second

---
+++ src/main/java/com/example/jenkindemo/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.jenkindemo.controller;
+
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("home")
+public class HomeController {
+	@GetMapping("/msg")
+	public String mess()
+	{
+		return "message fromm rohan";
+	}
+
+}
